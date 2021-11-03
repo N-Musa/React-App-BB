f7e8239cebc3e994925ffc48e64b46b34abf134a
N-Musa <n.musa@gmx.net>
2021-11-03 13:20:31 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,33 @@
-import React, { useState, useEffect } from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { SearchBar } from "./components/SearchBar";
+import { CharData } from "./components/CharData";
 
-function App() {
-  return(
-    <>
-    <Nav />
-      </>
-  )
- 
-}
+const App = () => {
+  const [char, setChar] = useState([]);
+  const [isPending, setIsPending] = useState(true);
+  const [query, setQuery] = useState("");
 
-export default App
+  useEffect(() => {
+    const fetchChar = async () => {
+      const res = await axios(
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
+      );
+
+      console.log(res.data);
+      setChar(res.data);
+      setIsPending(false);
+    };
+    fetchChar();
+  }, [query]);
+
+  return (
+    <div className="wrapper">
+      <SearchBar getQueryText={(queryText) => setQuery(queryText)} />
+
+      <CharData isPending={isPending} char={char} />
+    </div>
+  );
+};
+
+export default App;
